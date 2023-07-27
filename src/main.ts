8f293098729c552a38d996be72ba8898ab3c956d
ssekridov@gmail.com <ssekridov@gmail.com>
2023-07-27 14:52:39 +0200

    feat: dotenv connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
