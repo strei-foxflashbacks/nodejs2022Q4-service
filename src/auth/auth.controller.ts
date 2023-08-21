85e6ac0a2ea1c96e0d044746b64091d8aa41a35d
ssekridov@gmail.com <ssekridov@gmail.com>
2023-08-21 22:18:21 +0200

    feat: custom status for signin, logout & refresh

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
 import { UserDto } from 'src/user/dto';
@@ -17,16 +17,19 @@ export class AuthController {
   }
 
   @Post('signin')
+  @HttpCode(HttpStatus.OK)
   async signin(@Body() dto: UserDto): Promise<Tokens> {
     return await this.authService.signin(dto);
   }
 
   @Post('logout')
+  @HttpCode(HttpStatus.OK)
   logout() {
     this.authService.logout();
   }
 
   @Post('refresh')
+  @HttpCode(HttpStatus.OK)
   refresh() {
     this.authService.refresh();
   }
